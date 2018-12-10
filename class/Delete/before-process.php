08369ef4301167a8fc6effcb67a43ea669d68c88
Daniel Ménard <daniel.menard@laposte.net>
2018-12-10 17:20:10 +0100

    Delete : amélioration / standardisation
    - nouvelle vue "before-process" pour le message "le traitement va
    commencer"
    - renomme la vue "results" en "after-process"

---
+++ class/Delete/before-process.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Batch\Delete;
+
+/**
+ * Message affichée lorsque le traitement commence.
+ *
+ * @var int $count
+ */
+?>
+<p>
+    Suppression de <b><?= $count ?> notice(s)</b>...
+</p>
