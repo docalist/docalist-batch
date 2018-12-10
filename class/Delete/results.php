e268c93e86fbcb1958989e2677ef6c75c178ef16
Daniel Ménard <daniel.menard@laposte.net>
2018-12-10 17:03:18 +0100

    Vue results : affiche le résultat du traitement.

---
+++ class/Delete/results.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Batch\Delete;
+
+/**
+ * Affiche le résultat du traitement.
+ *
+ * @var int $count
+ */
+?>
+<p>
+    Terminé, <b><?= $count ?> notice(s)</b> ont été supprimée(s).
+</p>
