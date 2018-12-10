db786c2051b67dfd868b6089c7f5bc81fcd976ac
Daniel Ménard <daniel.menard@laposte.net>
2018-12-10 23:53:51 +0100

    Vue ChangeAuthor/confirm

---
+++ class/ChangeAuthor/confirm.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Batch\ChangeAuthor;
+
+use Docalist\Batch\Batch;
+use Docalist\Data\Field\PostAuthorField;
+use Docalist\Forms\Container;
+
+/**
+ * Demande confirmation à l'utilisateur.
+ *
+ * @var Batch   $this   Le traitement par lot en cours d'exécution.
+ * @var int     $count  Nombre de notices qui vont être traitées.
+ * @var string  $form   Le formulaire de paramétrage à afficher.
+ */
+?>
+<form method="post" action="">
+    <p><?php
+        $user = get_user_by('ID', $this->getParameter('createdBy'));
+        printf(
+            __('Vous allez <strong>attribuer %d notice(s)</strong> à <b>%s (%s)</b>.', 'docalist-batch'),
+            $count,
+            $user->display_name,
+            $user->user_login
+        ) ?>
+    </p>
+
+    <div style="display: none">
+        <?= $form ?>
+    </div>
+
+    <p>
+        <?= $this->backToSearchButton() ?>
+        <?= $this->backButton() ?>
+        <?= $this->confirmButton() ?>
+    </p>
+
+    <?= $this->silentInput() ?>
+    <?= $this->silentInput('silent2') ?>
+</form>
