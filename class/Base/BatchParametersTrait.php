033f11701cdd181c9bbd9f91c1b7226cd34c2e7e
Daniel Ménard <daniel.menard@laposte.net>
2018-12-10 20:53:53 +0100

    BatchParametersTrait : implementation de l'interface BatchParameters.

---
+++ class/Base/BatchParametersTrait.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Batch\Base;
+
+use Docalist\Batch\BatchParameters;
+
+/**
+ * Implementation standard de l'interface BatchParameters.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+trait BatchParametersTrait // implements BatchParameters
+{
+    /**
+     * Les paramètres en cours.
+     *
+     * @var array
+     */
+    private $parameters = [];
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setParameters(array $parameters): void
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function hasParameter(string $parameter): bool
+    {
+        return !empty($this->parameters[$parameter]);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getParameter(string $parameter, $default = null)
+    {
+        return empty($this->parameters[$parameter]) ? $default : $this->parameters[$parameter];
+    }
+}
