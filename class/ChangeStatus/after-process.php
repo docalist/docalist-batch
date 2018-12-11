dcf235a419eb1d6fd103047b56cf5cf9dcd94a15
Daniel Ménard <daniel.menard@laposte.net>
2018-12-11 12:21:15 +0100

    ChangeStatus/after-process : résultat du traitement.

---
+++ class/ChangeStatus/after-process.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Batch\ChangeStatus;
+
+use Docalist\Batch\Batch;
+
+/**
+ * Affiche le résultat du traitement.
+ *
+ * @var Batch $this Le traitement par lot en cours d'exécution.
+ * @var int $count  Le nombre de notices modifiées.
+ */
+?>
+<p>
+    <b><?= sprintf(__('Terminé, le statut a été changé dans %d notice(s).', 'docalist-batch'), $count) ?></b>
+</p>
