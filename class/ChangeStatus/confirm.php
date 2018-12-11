23e935bcf2ddfd82cdfb3e9feef84efa5ae99082
Daniel Ménard <daniel.menard@laposte.net>
2018-12-11 12:14:42 +0100

    ChangeStatus/confirm : demande confirmation à l'utilisateur.

---
+++ class/ChangeStatus/confirm.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Batch\ChangeStatus;
+
+use Docalist\Batch\Batch;
+use Docalist\Data\Field\PostAuthorField;
+use Docalist\Forms\Container;
+
+/**
+ * Demande confirmation à l'utilisateur.
+ *
+ * @var Batch   $this   Le traitement par lot en cours d'exécution.
+ * @var int     $count  Nombre de notices qui vont être traitées.
+ * @var string  $form   Le formulaire de paramétrage à afficher.
+ */
+?>
+<form method="post" action="">
+    <p><?php
+        $status = $this->getParameter('status');
+        printf(
+            __('Vous allez passer <strong>%d notice(s)</strong> en statut <b>%s</b>.', 'docalist-batch'),
+            $count,
+            $status
+        ) ?>
+    </p>
+
+    <div style="display: none">
+        <?= $form ?>
+    </div>
+
+    <p>
+        <?= $this->backToSearchButton() ?>
+        <?= $this->backButton() ?>
+        <?= $this->confirmButton() ?>
+    </p>
+
+    <?= $this->silentInput() ?>
+    <?= $this->silentInput('silent2') ?>
+</form>
