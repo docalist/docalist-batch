d6017970f1ea8e75eda02b1ab223ce016aa48c29
Daniel Ménard <daniel.menard@laposte.net>
2018-12-13 16:13:23 +0100

    BatchMoveToDatabase/after-process : résultat du traitement.

---
+++ class/MoveToDatabase/after-process.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+namespace Docalist\Batch\MoveToDatabase;
+
+use Docalist\Batch\Batch;
+
+/**
+ * Affiche le résultat du traitement.
+ *
+ * @var Batch $this Le traitement par lot en cours d'exécution.
+ * @var int $count  Le nombre de notices modifiées.
+ */
+?>
+<p>
+    <b><?= sprintf(__('Terminé, %d notice(s) transférée(s).', 'docalist-batch'), $count) ?></b>
+</p>
