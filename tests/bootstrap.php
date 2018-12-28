4869df6ceb7e52dae7a61b23b5ef313522fe0723
Daniel Ménard <daniel.menard@laposte.net>
2018-12-28 10:57:39 +0100

    Mise en place des tests unitaires

---
+++ tests/bootstrap.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of Docalist Batch.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+namespace Docalist\Batch\Tests;
+
+// Environnement de test
+$GLOBALS['wp_tests_options'] = [
+    'active_plugins' => [
+        'docalist-core/docalist-core.php',
+        'docalist-data/docalist-data.php',
+        'docalist-search/docalist-search.php',
+        'docalist-batch/docalist-batch.php',
+    ],
+];
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// wordpress-tests doit être dans le include_path de php
+// sinon, modifier le chemin d'accès ci-dessous
+require_once 'wordpress-develop/tests/phpunit/includes/bootstrap.php';
